66f7ac97378193eb75bf2b3d62a3d98180cc2ac7
kuma <kuma@augusta-inc.co.jp>
2018-09-26 11:04:19 +0900

    Override constractor on Food and Drink class

--- study5/food.py
+++ study5/food.py
@@ -1,6 +1,10 @@
 from menu_item import MenuItem
 
 class Food(MenuItem):
+    def __init__(self, name, price, calorie):
+        super().__init__(name, price)
+        self.calorie = calorie
+
     def info(self):
         return self.name + ': ¥' + str(self.price) + ' (' + str(self.calorie) + 'kcal)'
 
