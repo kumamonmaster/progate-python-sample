558535c970afbed2fd45b7d081021a4088fd3674
kuma <kuma@augusta-inc.co.jp>
2018-09-26 10:03:32 +0900

    Add constructor

--- study4/script.py
+++ study4/script.py
@@ -1,4 +1,8 @@
 class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
     def info(self):
         return self.name + ': ¥' + str(self.price)
 
@@ -6,12 +10,9 @@ class MenuItem:
         total_price = self.price * count
         return total_price
 
-menu_item1 = MenuItem()
-menu_item1.name = 'サンドイッチ'
-menu_item1.price = 500
+menu_item1 = MenuItem('サンドイッチ', 500)
 
 print(menu_item1.info())
 
 result = menu_item1.get_total_price(4)
-
 print('合計は' + str(result) + '円です')
