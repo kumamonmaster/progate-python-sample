f9a09b9223d94e590eac8698d634e8f73b0ad58d
kuma <kuma@augusta-inc.co.jp>
2018-09-26 10:54:46 +0900

    Override info method on Food class

--- study5/script.py
+++ study5/script.py
@@ -2,7 +2,6 @@ from food import Food
 from drink import Drink
 
 food1 = Food('サンドイッチ', 500)
-print(food1.info())
+food1.calorie = 330
 
-drink1 = Drink('コーヒー', 300)
-print(drink1.info())
+print(food1.info())
