0fc9d9e4897062b6d809ff5c70087590a77b040b
kuma <kuma@augusta-inc.co.jp>
2018-09-26 10:13:11 +0900

    Display menu list

--- study4/script.py
+++ study4/script.py
@@ -1,8 +1,14 @@
 from menu_item import MenuItem
 
 menu_item1 = MenuItem('サンドイッチ', 500)
+menu_item2 = MenuItem('チョコケーキ', 400)
+menu_item3 = MenuItem('コーヒー', 300)
+menu_item4 = MenuItem('オレンジジュース', 200)
 
-print(menu_item1.info())
+menu_items = [menu_item1, menu_item2, menu_item3, menu_item4]
 
-result = menu_item1.get_total_price(4)
-print('合計は' + str(result) + '円です')
+index = 0
+
+for menu_item in menu_items:
+    print(str(index) + '. ' + menu_item.info())
+    index += 1
